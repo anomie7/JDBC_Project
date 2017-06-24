5983aa4b7a2b6ba1a682661084b9492acc804a52
minu youn <anomie7777@gmail.com>
2017-06-24 21:10:20 +0900

    test

--- JDBC_Oracl01/src/OracleManager.java
+++ JDBC_Oracl01/src/OracleManager.java
@@ -51,7 +51,7 @@ public class OracleManager {
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(sql);
 			
-			System.out.println("��� \t �̸� \t ��å \t ��� \t �Ի��� \t �޿� \t �󿩱� \t �μ���ȣ \t");
+			System.out.println("사번 \t 이름 \t 직책 \t 상관 \t 입사일 \t 급여 \t 상여급 \t 부서번호 \t");
 			System.out.println("--------------------------------------------------");
 			
 			while(rs.next()){
@@ -99,9 +99,9 @@ public class OracleManager {
 			int rowCount = pstmt.executeUpdate();
 			
 			if(rowCount == 1){
-				System.out.println(ename + "��(��) �߰��Ǿ��ϴ�.");
+				System.out.println(ename + "이(가) 추가되었습니다.");
 			}else{
-				System.out.println("������ �߰��� �����Ͽ���ϴ�.");
+				System.out.println("데이터 추가에 실패하였습니다.");
 			}
 		} catch (Exception e) {
 			System.out.println(e);
@@ -127,9 +127,9 @@ public class OracleManager {
 			int rowCount = pstmt.executeUpdate();
 			
 			if(rowCount == 1){
-				System.out.println(eno + "�� ����Ǿ��ϴ�.");
+				System.out.println(eno + "이 삭제되었습니다.");
 			}else{
-				System.out.println("������ ����� �����Ͽ���ϴ�.");
+				System.out.println("데이터 삭제에 실패하였습니다.");
 			}
 		} catch (Exception e) {
 			System.out.println(e);
@@ -163,9 +163,9 @@ public class OracleManager {
 			int rowCount = pstmt.executeUpdate();
 			
 			if(rowCount == 1){
-				System.out.println(ename + "���� ����� ����Ǿ��ϴ�.");
+				System.out.println(ename + "님의 정보가 수정되었습니다.");
 			}else {
-				System.out.println("��� ����");
+				System.out.println("수정 실패");
 			}
 		}catch(Exception e){
 			System.out.println(e);
@@ -184,4 +184,3 @@ public class OracleManager {
 
 	}
 }
-
